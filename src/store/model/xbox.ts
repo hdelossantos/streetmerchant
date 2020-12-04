fbff00e5126efb5741a1f4374dd57beb2a0a1444
Hanly De Los Santos <lableupunn@gmail.com>
2020-12-04 18:28:34 -0500

    Adding inStock definition as well as xbox series s

--- src/store/model/xbox.ts
+++ src/store/model/xbox.ts
@@ -2,10 +2,15 @@ import {Store} from './store';
 
 export const Xbox: Store = {
 	labels: {
+		inStock: {
+			container:
+				'#BodyContent > section > div > div > div > div > div > div.pb-4.col-md-3 > button',
+			text: ['Checkout']
+		},
 		outOfStock: {
 			container:
 				'.src-pages-BundleBuilder-components-BundleBuilderHeader-__BundleBuilderHeader-module___checkoutButton',
-			text: ['out of stock']
+			text: ['Out of stock']
 		}
 	},
 	links: [
@@ -14,6 +19,12 @@ export const Xbox: Store = {
 			model: 'xbox series x',
 			series: 'xboxsx',
 			url: 'https://www.xbox.com/en-us/configure/8WJ714N3RBTL'
+		},
+		{
+			brand: 'microsoft',
+			model: 'xbox series s',
+			series: 'xboxss',
+			url: 'https://www.xbox.com/en-us/configure/942J774TP9JN'
 		}
 	],
 	name: 'xbox'
